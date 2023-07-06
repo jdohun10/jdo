d45e291c1a045fb0c23710e9a503f068cebdc637
jdohun10 <126150524+jdohun10@users.noreply.github.com>
2023-07-06 23:11:50 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -3,8 +3,8 @@ api_token = ""
 api2 = "" # 계좌 자충 API 주소
 api2_token = "" # 계좌 자충 API 키
 
-token = "" #봇토큰
-license_master_ids = [837944214708944896, 610961244005990506] #라이센스 생성권한
+token = "MTEyMTA3NDIzODI2NzMzODg2Mg.GfNURA.ep8WvrFMvGkblaV1EbVdsbpuvSiR1SgO0pD8cw" #봇토큰
+license_master_ids = [1125035794575413268, 610961244005990506] #라이센스 생성권한
 admin_id = 837944214708944896
 webhook_name = "Roy Button" #웹훅이름
 webhook_profile_url = "https://cdn.discordapp.com/attachments/569848215353032704/962372967612960828/Roy_Vending.png" #웹훅 프로필 사진
